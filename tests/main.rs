1894dcb0da1c55ad5cb2091225e31cbc22ab1c3e
Ain Tohvri <ain@flashbit.net>
2023-01-31 00:15:20 +0100

    Reduce sample JSON size

--- tests/main.rs
+++ tests/main.rs
@@ -6,6 +6,6 @@ mod tests {
     fn check_network_count() {
         let url = "https://raw.githubusercontent.com/ain/cidranger/main/tests/data/sample.json";
         let ipnet = parser::parse_ips(url);
-        assert_eq!(200, ipnet.expect("correct network count").len());
+        assert_eq!(6, ipnet.expect("correct network count").len());
     }
 }
