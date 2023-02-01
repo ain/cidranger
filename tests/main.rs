b13afe5e130c59c9adacada0104d35f7e1e7fabd
Ain Tohvri <ain@flashbit.net>
2023-02-01 23:16:59 +0100

    Move test to strict type

--- tests/main.rs
+++ tests/main.rs
@@ -2,10 +2,12 @@
 
 mod tests {
 
+    use ipnet::IpNet;
+
     #[test]
     fn check_network_count() {
         let url = "https://raw.githubusercontent.com/ain/cidranger/main/tests/data/sample.json";
-        let ipnet = parser::parse_ips(url);
+        let ipnet: Option<Vec<IpNet>> = parser::parse_ips(url);
         assert_eq!(6, ipnet.expect("correct network count").len());
     }
 }
