d456bcafff92c4fbc5e713fb6dd40df6cd5ee0df
Ain Tohvri <ain@flashbit.net>
2023-02-01 23:37:48 +0100

    Update test to check exact parser response

--- tests/main.rs
+++ tests/main.rs
@@ -5,9 +5,9 @@ mod tests {
     use ipnet::IpNet;
 
     #[test]
-    fn check_network_count() {
+    fn check_parse_ips() {
         let url = "https://raw.githubusercontent.com/ain/cidranger/main/tests/data/sample.json";
         let ipnet: Option<Vec<IpNet>> = parser::parse_ips(url);
-        assert_eq!(6, ipnet.expect("correct network count").len());
+        assert_eq!("2001:4860:4801:10::/64, 2001:4860:4801:c::/64, 2001:4860:4801:f::/64, 34.100.182.96/28, 34.146.150.144/28, 66.249.79.96/27", ipnet.unwrap().iter().map(|range| range.to_string()).collect::<Vec<String>>().join(", "));
     }
 }
