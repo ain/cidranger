8a6b8004345cbe66e5a7a8fa0a31754b9aae7492
Ain Tohvri <ain@flashbit.net>
2023-02-03 17:29:24 +0100

    Add clap for basic CLI implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+extern crate clap;
+
 use parser::parse_ips;
 use ipnet::IpNet;
+use clap::Parser;
+use std::net::IpAddr;
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    #[arg(short, long, value_parser = clap::value_parser!(IpAddr))]
+    ip: String,
+    #[arg(short,long)]
+    ranges_url: String,
+    #[arg(short,long)]
+    filter_key: Option<String>
+}
 
 fn main() {
 
-    let googlebot_ip_range_url: &'static str = "https://developers.google.com/static/search/apis/ipranges/googlebot.json";
+    let cli = Cli::parse();
 
-    //let arg = std::env::args()
-        //.skip(1)
-        //.next()
-        //.expect("one argument required");
+    let googlebot_ip_range_url: &'static str = "https://developers.google.com/static/search/apis/ipranges/googlebot.json";
 
     let google_ip_ranges: Vec<IpNet> = parse_ips(googlebot_ip_range_url).unwrap();
 
